27a5234b81cc62940350d3ac11a6f882c9ce08aa
Anita Samuel <migist2@gmail.com>
2025-07-11 02:27:19 +0100

    extend teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -16,3 +16,19 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+
+//task 1b
+
+interface Directors extends Teacher {
+    numberOfReports: number
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
