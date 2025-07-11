34d301982645c7dd7a26f9118d550111d3176358
Anita Samuel <migist2@gmail.com>
2025-07-11 02:42:37 +0100

    print teacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,10 +1,10 @@
 interface Teacher {
-    readonly firstName: string,
-    readonly lastName: string,
-    fullTimeEmployee: boolean,
-    yearsOfExperience?: number,
-    location: string,
-    [propName: string]: any
+  readonly firstName: string,
+  readonly lastName: string,
+  fullTimeEmployee: boolean,
+  yearsOfExperience?: number,
+  location: string,
+  [propName: string]: any
 }
 
 const teacher3: Teacher = {
@@ -21,7 +21,7 @@ console.log(teacher3);
 //task 1b
 
 interface Directors extends Teacher {
-    numberOfReports: number
+  numberOfReports: number
 }
 
 const director1: Directors = {
@@ -32,3 +32,13 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+//function
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string
+}
+
+const printTeacher: PrintTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}}`;
+}
+console.log(printTeacher("John", "Doe"))
